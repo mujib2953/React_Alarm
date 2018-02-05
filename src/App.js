4845ca87379b4ab344fc1a3dc30b856f599d435f
Mujib Ansari <mujibur@sportzinteractive.net>
2018-02-05 18:34:21 +0530

    basic structure created.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
+// ---- common components
+import {
+	TabComp
+} from './component/common';
+
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				Hello World...
+				
 			</div>
 		);
 	}
